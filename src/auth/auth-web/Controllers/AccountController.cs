c342df56f696bee55eeee5c4588d4a3ce1aff3bd
Matt Cosand <matt@cosand.net>
2016-03-25 22:59:25 -0700

    split controller. Move inline script to resource.

--- src/auth/auth-web/Controllers/AccountController.cs
+++ src/auth/auth-web/Controllers/AccountController.cs
@@ -1,16 +1,9 @@
 ﻿/*
  * Copyright Matthew Cosand
  */
-using System.Threading.Tasks;
-using System.Web;
 using System.Web.Mvc;
-using IdentityServer3.Core;
-using IdentityServer3.Core.Extensions;
 using IdentityServer3.Core.Models;
 using IdentityServer3.Core.ViewModels;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
-using Sar.Auth.Models;
 using Sar.Auth.Services;
 
 namespace Sar.Auth.Controllers
@@ -22,66 +15,6 @@ namespace Sar.Auth.Controllers
     public AccountController(SarUserService service)
     {
       _userService = service;
-    }
-
-    // GET: Account
-    [Route(AuthWebApplication.SITEROOT + "register")]
-    public ActionResult Register()
-    {
-      return View();
-    }
-
-    [HttpGet]
-    [Route(AuthWebApplication.SITEROOT + "registerlogin")]
-    public async Task<ActionResult> RegisterLogin()
-    {
-      // this verifies that we have a partial signin from idsvr
-      var ctx = Request.GetOwinContext();
-      var partial_login = await ctx.Environment.GetIdentityServerPartialLoginAsync();
-      
-      if (partial_login == null)
-      {
-        return View("Error");
-      }
-
-      ViewBag.Email = (partial_login.FindFirst(Constants.ClaimTypes.Email)
-                   ?? partial_login.FindFirst("upn"))?.Value;
-      ViewBag.Name = partial_login.FindFirst(Constants.ClaimTypes.Name)?.Value ?? "Unknown User";
-
-      return View();
-    }
-    [HttpPost]
-    [Route(AuthWebApplication.SITEROOT + "externalVerificationCode")]
-    public async Task<ActionResult> ExternalVerificationCode(VerifyCodeRequest request)
-    {
-      var ctx = Request.GetOwinContext();
-      var partial_login = await ctx.Environment.GetIdentityServerPartialLoginAsync();
-      if (partial_login == null)
-      {
-        return View("Error");
-      }
-
-      await _userService.SendExternalVerificationCode(partial_login, request.Email);
-
-      //using (var db = )
-      return CamelJson(new { Success = true });
-    }
-
-    [HttpPost]
-    [Route(AuthWebApplication.SITEROOT + "verifyExternalCode")]
-    public async Task<ActionResult> VerifyExternalCode(VerifyCodeRequest request)
-    {
-      var ctx = Request.GetOwinContext();
-      var partial_login = await ctx.Environment.GetIdentityServerPartialLoginAsync();
-      if (partial_login == null)
-      {
-        return View("Error");
-      }
-
-      await _userService.VerifyExternalCode(partial_login, request.Email, request.Code);
-
-      var resumeUrl = await ctx.Environment.GetPartialLoginResumeUrlAsync();
-      return CamelJson(new { Success = true, Url = resumeUrl });
     }
 
     #region Login
@@ -188,13 +121,6 @@ namespace Sar.Auth.Controllers
     public virtual ActionResult Error(ErrorViewModel model)
     {
       return this.View(model);
-    }
-
-    private ContentResult CamelJson(object o)
-    {
-      var camelCaseFormatter = new JsonSerializerSettings();
-      camelCaseFormatter.ContractResolver = new CamelCasePropertyNamesContractResolver();
-      return Content(JsonConvert.SerializeObject(o, camelCaseFormatter), "application/json");
     }
   }
 }
---
+++ src/auth/auth-web/Controllers/RootController.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Copyright Matthew Cosand
+ */
+using System.Web.Mvc;
+
+namespace Sar.Auth.Controllers
+{
+  public class RootController : Controller
+  {
+    [Route(AuthWebApplication.SITEROOT + "jsconfig")]
+    [HttpGet]
+    public ContentResult JSConfig()
+    {
+      return Content(string.Format("window.appRoot = '{0}';", Url.Content("~/")), "text/javascript");
+    }
+  }
+}
